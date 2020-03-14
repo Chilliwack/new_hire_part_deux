086654284c0fbaada3540b45adceb772b0017e5f
Chilliwack <46973804+Chilliwack@users.noreply.github.com>
2020-03-13 18:40:49 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ For more information, see https://stackoverflow.com/questions/1471994/what-is-se
 from setuptools import setup, find_packages
 PACKAGES = find_packages()
 
-opts = dict(name='New_hire',
+opts = dict(name='new_hire',
             maintainer='Chilliwack',
-            maintainer_email='your_email',
-            description='New hire is a script and set of functions that intergrates with the Hutchs Toolbox system',
-            long_description=("""New hire is a script and set of functions that intergrates with the Hutchs Toolbox system to make it easier to identify new users."""),
+            maintainer_email='chilliwack@nowhere.com',
+            description='new_hire is a script and set of functions that intergrates with the Hutchs Toolbox system',
+            long_description=("""new_hire is a script and set of functions that intergrates with the Hutchs Toolbox system to make it easier to identify new users."""),
             url='https://github.com/FredHutch/new_data_employee',
             license='MIT', # default license, change here and in the git repo if using a different license
             author='Chilliwack',
-            author_email='your_email',
+            author_email='chilliwack@nowhere.com',
             version='0.1',
             packages=PACKAGES
            )
